95b78e4d6b8c7d8885cace77caac9109bfcc4cd4
Maria Fernanda Roco <fernanda.roco1@icloud.com>
2016-11-15 08:08:49 -0400

    Update proyecto1.py

--- proyecto1.py
+++ proyecto1.py
@@ -1,7 +1,8 @@
 import random
 Def GeneraListaAleatoria(n):
-  lista=[0]*n
-  for i in range (n):
+  lista=[0]*6
+  for i in range (6):
     lista [i]=random.randint(1,36)
   return lista
+
 print GeneraListaAleatoria ()
